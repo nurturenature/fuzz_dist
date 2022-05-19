33ebf2da6eb0224774e87922430f4be38fa4c0b5
NurtureNature <jeff.suttor@earthlink.net>
2022-05-19 11:28:24 -0700

    refactor to jepse.nemesis.combined exclusively

--- jepsen.fuzz_dist/src/fuzz_dist/core.clj
+++ jepsen.fuzz_dist/src/fuzz_dist/core.clj
@@ -1,47 +1,55 @@
 (ns fuzz-dist.core
   (:require
    [clojure.string :as str]
-   [clojure.tools.logging :refer :all]
-   [fuzz-dist
-    [db :as db]
-    [nemesis :as nemesis]
-    [util :as util]]
+   [fuzz-dist.db :as db]
    [fuzz-dist.workload [g-set :as g-set]]
    [jepsen
     [cli :as cli]
     [checker :as checker]
     [generator :as gen]
     [tests :as tests]]
+   [jepsen.nemesis.combined :as nc]
    [jepsen.checker.timeline :as timeline]
    [jepsen.os.debian :as debian]))
-
-(defn g-set-read [_ _]
-  (repeat {:type :invoke, :f :read, :value nil}))
-(defn g-set-add  [_ _]
-  (map (fn [x] {:type :invoke, :f :add, :value (str x)}) (range)))
-
-(defn combine-workload-package-generators
-  "Constructs a test generator by combining workload and package generators
-   configures with CLI test opts"
-  [opts workload package]
-
-  (gen/phases
-   (->> (:generator workload)
-        (gen/stagger (/ (util/rand-int-from-range (:rate opts))))
-        (gen/nemesis (:generator package))
-        (gen/time-limit (:time-limit opts)))
-
-   (:final-generator package)
-
-   (:final-generator workload)))
 
 (def workloads
   "A map of workload names to functions that construct workloads, given opts."
   {:g-set g-set/workload})
 
-(def packages
-  "A map of package names to functions that construct packages, given opts."
-  {:g-set g-set/package})
+(def nemeses
+  "The types of faults our nemesis can produce"
+  #{:partition})
+
+(def special-nemeses
+  "A map of special nemesis names to collections of faults"
+  {:none      []
+   :standard  [:partition]
+   :all       [:partition]})
+
+(def partition-targets
+  "Valid targets for partition nemesis operations."
+  #{:majority :minority-third :one :majority-ring :primaries})
+
+(def standard-nemeses
+  "A collection of partial options maps for various nemeses we want to run as a
+  part of test-all."
+  [{:nemesis nil}
+   {:nemesis #{:partition}}])
+
+(defn combine-workload-package-generators
+  "Constructs a test generator by combining workload and package generators
+   configured with CLI test opts"
+  [opts workload package]
+
+  (gen/phases
+   (->> (:generator workload)
+        (gen/stagger (/ (:rate opts)))
+        (gen/nemesis (:generator package))
+        (gen/time-limit (:time-limit opts)))
+
+   (gen/nemesis (:final-generator package))
+
+   (:final-generator workload)))
 
 (defn fuzz-dist-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
@@ -50,22 +58,26 @@
   (let [db            (db/db :git)
         workload-name (:workload opts)
         workload      ((workloads workload-name) opts)
-        package       ((packages  workload-name) opts)]
+        package       (nc/nemesis-package
+                       {:db        db
+                        :nodes     (:nodes opts)
+                        :faults    (:nemesis opts)
+                        :partition {:targets (:partition-targets opts)}
+                        :interval  (:nemesis-interval opts)})]
 
     (merge tests/noop-test
            opts
            {:name       (str "fuzz-dist"
                              "-Antidote"
                              "-" (count (:nodes opts)) "xdc"
-                             "-" (str (seq (:faults opts))  "-at-" (util/pprint-ranges (:faults-times opts)) "s")
+                             "-" (str (seq (:nemesis opts))  "-at-" (:nemesis-interval opts) "s")
                              "-for-" (:time-limit opts) "s"
-                             "-" (util/pprint-range (:rate opts)) "ts"
+                             "-" (:rate opts) "ts"
                              "-" workload-name)
             :os         debian/os
             :db         db
             :client     (:client workload)
             :nemesis    (:nemesis package)
-            :pure-generators true
             :generator  (combine-workload-package-generators opts workload package)
             :checker    (checker/compose
                          {:workload   (:checker workload)
@@ -78,6 +90,24 @@
                           ;; :logs       (checker/log-file-pattern #"ERROR" ".log")
                           })})))
 
+(def validate-non-neg
+  [#(and (number? %) (not (neg? %))) "Must be non-negative"])
+
+(defn parse-comma-kws
+  "Takes a comma-separated string and returns a collection of keywords."
+  [spec]
+  (->> (str/split spec #",")
+       (remove #{""})
+       (map keyword)))
+
+(defn parse-nemesis-spec
+  "Takes a comma-separated nemesis string and returns a collection of keyword
+  faults."
+  [spec]
+  (->> (parse-comma-kws spec)
+       (mapcat #(get special-nemeses % [%]))
+       set))
+
 (def test-opt-spec
   "Options for tests."
   [["-w" "--workload NAME" "What workload to run."
@@ -87,63 +117,39 @@
 
 (def opt-spec
   "Additional command line options."
-  [[nil "--faults FAULTS" "A comma-separated list of specific faults to inject."
-    :default #{:all}
-    :parse-fn (fn [string]
-                (->> (str/split string #"\s*,\s*")
-                     (map keyword)
-                     set))
-    :validate [(partial every? nemesis/all-nemeses) (cli/one-of nemesis/all-nemeses)]]
 
-   [nil "--faults-times [[QT,QT] [DT,DT]]" "Range of [[QuietMin, QuietMax] [DurationMin, DurationMax]] times."
-    :default  [[1,5] [4,6]]
+  [[nil "--nemesis FAULTS" "A comma-separated list of nemesis faults to enable"
+    :default (:all special-nemeses)
+    :parse-fn parse-nemesis-spec
+    :validate [(partial every? (into nemeses (keys special-nemeses)))
+               (str (cli/one-of nemeses) ", or " (cli/one-of special-nemeses))]]
+
+   [nil "--nemesis-interval SECONDS" "How long to wait between nemesis faults."
+    :default  15
     :parse-fn read-string
-    ;; TODO :validate [#(and (number? %) (pos? %)) "Must be a positive number"]
-    ]
+    :validate [#(and (number? %) (pos? %)) "must be a positive number"]]
 
-   ["-r" "--rate [HZ,HZ]" "Range of approximate number of requests per second, per thread."
-    :default  [10,10]
+   [nil "--partition-targets TARGETS" "A comma-separated list of nodes to target for network partitions; e.g. one,all"
+    :default (vec partition-targets)
+    :parse-fn parse-comma-kws
+    :validate [(partial every? partition-targets) (cli/one-of partition-targets)]]
+
+   [nil "--rate HZ" "Target number of ops/sec"
+    :default  10
     :parse-fn read-string
-    ;; TODO :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
-    ]])
-
-(defn parse-faults
-  "Post-processes :faults #{:all}, #{:none}."
-  [parsed]
-  (let [options (:options parsed)
-        faults  (:faults options)]
-    (assoc parsed :options (-> options
-                               (assoc :faults (case faults
-                                                #{:all} (set (keys nemesis/all-nemeses))
-                                                #{:none} #{}
-                                                faults))))))
-
-(defn opt-fn
-  "Post-processes the parsed CLI options structure."
-  [parsed]
-  (-> parsed
-      parse-faults
-      cli/test-opt-fn))
+    :validate validate-non-neg]])
 
 (defn all-tests
   "Takes parsed CLI options and constructs a sequence of test options, by
-  combining all workloads, faults, fault durations, and rates."
+  combining all workloads, faults."
   [opts]
-  (let [faults                (:faults opts)
-        [[quiet-min,quiet-max]
-         [dur-min,dur-max]]   (:faults-times opts)
-        quiets                (range quiet-min (+ quiet-max 1))
-        durations             (range dur-min   (+ dur-max 1))
-        [rate-min,rate-max]   (:rate opts)
-        rates                 (range rate-min  (+ rate-max 1))
-        workloads             [(:workload opts)]
+  (let [faults                (:nemesis opts)
+        workloads             (:workload opts)
         counts                (range (:test-count opts))]
-    (->> (for [w workloads, f faults, q quiets, d durations, r rates, i counts]
+    (->> (for [w workloads, f faults, i counts]
            (assoc opts
                   :workload w
-                  :faults #{f}
-                  :faults-times [[q,q] [d,d]]
-                  :rate [r,r]))
+                  :nemesis #{f}))
          (map fuzz-dist-test))))
 
 (defn -main
@@ -151,10 +157,8 @@
                 browsing results."
   [& args]
   (cli/run! (merge (cli/single-test-cmd {:test-fn  fuzz-dist-test
-                                         :opt-spec (concat test-opt-spec opt-spec)
-                                         :opt-fn*  opt-fn})
+                                         :opt-spec (concat test-opt-spec opt-spec)})
                    (cli/test-all-cmd    {:tests-fn all-tests
-                                         :opt-spec (concat test-opt-spec opt-spec)
-                                         :opt-fn*  opt-fn})
+                                         :opt-spec (concat test-opt-spec opt-spec)})
                    (cli/serve-cmd))
             args))
