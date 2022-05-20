43d6da448d3a7e6d828431435ec879ac22bec39d
NurtureNature <jeff.suttor@earthlink.net>
2022-05-20 14:05:02 -0700

    better client exception handling

--- jepsen.fuzz_dist/src/fuzz_dist/client.clj
+++ jepsen.fuzz_dist/src/fuzz_dist/client.clj
@@ -1,6 +1,8 @@
 (ns fuzz-dist.client
   (:require [cheshire.core :as json]
-            [manifold.stream :as s]))
+            [clojure.tools.logging :refer :all]
+            [manifold.stream :as s])
+  (:use [slingshot.slingshot :only [try+]]))
 
 (defn node-url
   "An HTTP url for connecting to a node's FuzzDist Elixir client."
@@ -11,13 +13,17 @@
   "Invokes the op over the ws connection.
   On the BEAM side a :cowboy_websocket_handler dispatches to an Elixir @behavior."
   [conn mod fun op]
-  (s/try-put! conn
-              (json/generate-string
-               {:mod mod
-                :fun fun
-                :args op})
-              5000)
-
-  (json/parse-string @(s/try-take! conn 60000) true)
-  ;; TODO: catch timeout
-  )
+  (try+
+   (s/try-put! conn
+               (json/generate-string
+                {:mod mod
+                 :fun fun
+                 :args op})
+               5000)
+    ;; TODO: catch timeouts
+   (json/parse-string @(s/try-take! conn 60000) true)
+    ;; TODO: catch timeouts
+   (catch Object e
+     ;; TODO: smarter Exception -> info/fail, e.g. Connection refused is a fail
+     (error (str "ws-invoke Exception:" e))
+     {:type "info" :error (.getCause e)})))
