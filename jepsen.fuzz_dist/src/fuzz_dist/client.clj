81b817f8c15bb61f97c0b69a76d8a09bcb9e6ba2
NurtureNature <jeff.suttor@earthlink.net>
2022-05-27 10:10:29 -0700

    websocket timeout, exception handling

--- jepsen.fuzz_dist/src/fuzz_dist/client.clj
+++ jepsen.fuzz_dist/src/fuzz_dist/client.clj
@@ -1,30 +1,41 @@
 (ns fuzz-dist.client
-  (:require [cheshire.core :as json]
+  (:require [aleph.http :as http]
+            [cheshire.core :as json]
             [clojure.tools.logging :refer :all]
             [manifold.stream :as s])
-  (:use [slingshot.slingshot :only [try+]]))
+  (:use [slingshot.slingshot :only [throw+]]))
 
 (defn node-url
   "An HTTP url for connecting to a node's FuzzDist Elixir client."
   [node]
   (str "ws://" node ":8080" "/fuzz_dist/jep_ws"))
 
+(defn get-ws-conn
+  "Returns a websocket connection as a bidirectional stream.
+  Exceptions are intentional to invalidate clients"
+  ([url node] (get-ws-conn url node 1000))
+  ([url node timeout]
+   (let [conn (deref (http/websocket-client (url node)) timeout nil)]
+     (if (not (nil? conn))
+       conn
+       (throw+ [:type :info :error "Websocket client open failed, timeout?"])))))
+
 (defn ws-invoke
   "Invokes the op over the ws connection.
-  On the BEAM side a :cowboy_websocket_handler dispatches to an Elixir @behavior."
-  ([conn mod fun op] (ws-invoke conn mod fun op 5000))
+  On the BEAM side, a :cowboy_websocket_handler dispatches to an Elixir @behavior.
+  Be conservative, throw Exceptions, let this client crash
+  to invalidate current op and get fresh client."
+  ([conn mod fun op] (ws-invoke conn mod fun op 1000))
   ([conn mod fun op timeout]
-   (try+
-    (s/try-put! conn
-                (json/generate-string
-                 {:mod mod
-                  :fun fun
-                  :args op})
-                5000)
-    ;; TODO: catch timeouts
-    (json/parse-string @(s/try-take! conn timeout) true)
-    ;; TODO: catch timeouts
-    (catch Exception e
-     ;; TODO: smarter Exception -> info/fail, e.g. Connection refused is a fail
-      (error (str "ws-invoke Exception:" e))
-      {:type "info" :error (.getCause e)}))))
+   (if (not @(s/try-put! conn
+                         (json/generate-string
+                          {:mod mod
+                           :fun fun
+                           :args op})
+                         timeout))
+     (throw+ [:type :info :error "Failed to put to websocket connection."]))
+
+   (let [resp @(s/try-take! conn timeout)]
+     (if (= nil resp)
+       (throw+ [:type :info :error "Failed to take from websocket connection."]))
+     (json/parse-string resp true))))
