8769986b4cbd1037b26bdc293d8c0946a6cee8b0
NurtureNature <jeff.suttor@earthlink.net>
2022-06-02 21:12:15 -0700

    update deps

--- jepsen.fuzz_dist/project.clj
+++ jepsen.fuzz_dist/project.clj
@@ -4,10 +4,10 @@
   :license {:name "Licensed under the Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[com.google.guava/guava "30.1-jre"]
-                 [org.clojure/clojure "1.11.0"]
+                 [org.clojure/clojure "1.11.1"]
                  [aleph "0.4.6"]
                  [cheshire "5.10.2"]
-                 [jepsen "0.2.6"]
+                 [jepsen "0.2.7-SNAPSHOT"]
                  [manifold "0.2.3"]]
   :main fuzz-dist.core
   :repl-options {:init-ns fuzz-dist.core}
