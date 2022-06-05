74bd4a6f19ce470cfe08db52fc9e26969f75cc04
NurtureNature <jeff.suttor@earthlink.net>
2022-06-05 11:46:15 -0700

    config for new AntidoteDb mono repo

--- beam.fuzz_dist/mix.exs
+++ beam.fuzz_dist/mix.exs
@@ -34,7 +34,12 @@ defmodule FuzzDist.MixProject do
 
   defp deps do
     [
-      {:antidotec_pb, git: "https://github.com/AntidoteDB/antidote-erlang-client.git"},
+      {:antidote_pb_codec,
+       git: "https://github.com/AntidoteDB/antidote.git",
+       subdir: "apps/antidote_pb_codec",
+       override: true},
+      {:antidotec_pb,
+       git: "https://github.com/AntidoteDB/antidote.git", subdir: "apps/antidote-erlang-client"},
       {:cowboy, "~> 2.9"},
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
