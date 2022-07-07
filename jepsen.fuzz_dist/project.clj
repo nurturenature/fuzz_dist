b65e292b308295dfbe33604b92b6d641f7ee9c13
NurtureNature <jeff.suttor@earthlink.net>
2022-07-07 14:22:05 -0700

    model all individual process states w/TreeRangeSet

--- jepsen.fuzz_dist/project.clj
+++ jepsen.fuzz_dist/project.clj
@@ -3,16 +3,17 @@
   :url "https://github.com/nurturenature"
   :license {:name "Licensed under the Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.google.guava/guava "30.1-jre"]
+  :dependencies [[com.google.guava/guava "31.1-jre"]
                  [org.clojure/clojure "1.11.1"]
                  [aleph "0.4.6"]
                  [cheshire "5.10.2"]
-                 [jepsen "0.2.7-SNAPSHOT"]
+                 [jepsen "0.2.7"]
                  [manifold "0.2.3"]]
   :main fuzz-dist.core
   :repl-options {:init-ns fuzz-dist.core}
   :plugins [[jonase/eastwood "1.2.3"]
-            [lein-codox "0.10.8"]]
+            [lein-codox "0.10.8"]
+            [lein-localrepo "0.5.4"]]
   :codox {:output-path "target/doc/"
           :source-uri "https://github.com/nurturenature/fuzz_dist/blob/{version}/jepsen.fuzz_dist/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
